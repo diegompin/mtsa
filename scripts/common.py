918c0d2b28288abad287d0a456a42b0b4d241858
diegompin <diegompin@gmail.com>
2023-02-24 22:19:43 +0000

    multiple runs

--- scripts/common.py
+++ scripts/common.py
@@ -2,15 +2,30 @@ import time
 from functools import partial
 from functools import reduce
 
+
 def elapsed_time(fun, *args):
     start = time.time()
     fun_return = fun(*args)
     end = time.time()
     time_elapsed = end - start
-    return time_elapsed, fun_return
+    result = {
+        "time_elapsed": time_elapsed,
+        "fun_return": fun_return
+    }
+    return result
+
 
 def multiple_runs(runs, fun, *args):
-    run_ids = range(runs)
+    list_run_ids = range(runs)
     # fun_partial = partial(elapsed_time, fun, *args)
-    multiple_results = map(lambda x: (x, elapsed_time(fun, *args)), run_ids)
+    # multiple_results = map(lambda x: (x, elapsed_time(fun, *args)), run_ids)
+
+    multiple_results = map(
+        lambda run_id:
+        {
+            'run_id': run_id,
+            "run_details": elapsed_time(fun, *args)
+        },
+        list_run_ids)
+
     return multiple_results
--- scripts/paper2023.py
+++ scripts/paper2023.py
@@ -1,26 +1,7 @@
 '''
 
 '''
-#Disable Logging
-import os
-import logging
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-import tensorflow as tf
-tf.get_logger().setLevel(logging.ERROR)
-tf.keras.utils.disable_interactive_logging()
-
-from common import elapsed_time, multiple_runs
-import argparse
-import os
-import numpy as np
-import itertools as ite
-import pandas as pd
-from functools import reduce, partial
-import glob
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.mixture import GaussianMixture
-from sklearn.model_selection import LeaveOneOut
+# Disable Logging
 from mtsa import (
     MFCCMix,
     Hitachi,
@@ -30,26 +11,43 @@ from mtsa import (
     get_tsne_results,
     files_train_test_split_combined
 )
+from sklearn.model_selection import LeaveOneOut
+from sklearn.mixture import GaussianMixture
+import seaborn as sns
+import matplotlib.pyplot as plt
+import glob
+from functools import reduce, partial
+import pandas as pd
+import itertools as ite
+import numpy as np
+import argparse
+from common import elapsed_time, multiple_runs
+import tensorflow as tf
+import os
+import logging
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.get_logger().setLevel(logging.ERROR)
+tf.keras.utils.disable_interactive_logging()
+import re
+import json
 
 class PaperScript2023:
 
     def __init__(self, *args, **kwargs):
         self.__dict__.update(kwargs)
 
-
     def results(self):
-        #TODO revert
         self.results_01_individual()
-        # self.results_02_tse()
-        # self.results_03_combined()
-   
+        self.results_02_tse()
+        self.results_03_combined()
+
     def get_features(self):
         number_features = np.arange(1, len(FEATURES)+1)
-        combinations = lambda r: ite.combinations(FEATURES, r)
+        def combinations(r): return ite.combinations(FEATURES, r)
         all_combinations = map(combinations, number_features)
-        features = reduce(lambda x,y: ite.chain(x,y), all_combinations)
+        features = reduce(lambda x, y: ite.chain(x, y), all_combinations)
         return features
-        
+
     def get_models_mfccmix(self):
         def build_mfccmix_by_features(features):
             mfccmix = MFCCMix(
@@ -60,44 +58,59 @@ class PaperScript2023:
         features = self.get_features()
         mfcc_models = map(build_mfccmix_by_features, features)
         return mfcc_models
-        
+
     def get_models_baseline(self):
         hitachi = [("Baseline", Hitachi())]
         return hitachi
-    
+
     def get_models(self):
         model_baseline = self.get_models_baseline()
         models_mfccmix = self.get_models_mfccmix()
-        all_models = reduce(lambda x,y: ite.chain(x,y), (model_baseline, models_mfccmix))
+        all_models = reduce(lambda x, y: ite.chain(
+            x, y), (model_baseline, models_mfccmix))
         return list(all_models)
 
     def get_paths(self, level):
         paths = glob.glob(os.path.join(self.path, f'*{os.sep}' * level))
         return paths
 
-    def write_df(self, df_final, **kwargs):
+    def write_dict(self, my_dict, **kwargs):
         path_output = os.path.join(self.output, 'rocs')
         if not os.path.exists(path_output):
             os.makedirs(path_output)
-        filename = 'roc'
+        filename = 'filename'
         if 'filename' in kwargs:
             filename = kwargs['filename']
         filepath = os.path.join(path_output, f'{filename}')
         with open(f'{filepath}.tex', 'w') as tf:
-            tf.write(df_final.to_latex())
-        df_final.to_hdf(f'{filepath}.h5', 'df')    
-    
+            json.dump(my_dict, tf)
+            
+    def write_df(self, df_final, **kwargs):
+        path_output = os.path.join(self.output, 'rocs')
+        if not os.path.exists(path_output):
+            os.makedirs(path_output)
+        filename = 'filename'
+        if 'filename' in kwargs:
+            filename = kwargs['filename']
+        filepath = os.path.join(path_output, f'{filename}')
+        with open(f'{filepath}.tex', 'w') as tf:
+            # tf.write(df_final.to_latex())
+            tf.write(df_final.style.to_latex())
+        df_final.to_hdf(f'{filepath}.h5', 'df')
+
     def convert_to_dataframe(self, rocs, level):
         rocs_norms = list(map(self.extract_params, [(r, level) for r in rocs]))
         # features = list(self.get_paths(level))]
         df = pd.DataFrame(rocs_norms)
         col_params = [f'P{p}' for p in range(level+1)]
         col_features = ['features', 'roc']
-        df_params = pd.DataFrame(df[0].tolist(), index=df.index, columns=col_params)
-        df_rocs = pd.DataFrame(df[[1,2]])
+        df_params = pd.DataFrame(
+            df[0].tolist(), index=df.index, columns=col_params)
+        df_rocs = pd.DataFrame(df[[1, 2]])
         df_rocs.columns = col_features
         df_final = pd.concat([df_params, df_rocs], axis=1)
-        df_final = pd.pivot(df_final, index=list(df_params.columns), columns='features', values='roc')
+        df_final = pd.pivot(df_final, index=list(
+            df_params.columns), columns='features', values='roc')
         df_final = df_final[sorted(df_final.columns, key=len)]
         df_final = df_final.loc[df_final.index].apply(lambda x: round(x, 2))
         return df_final
@@ -109,37 +122,60 @@ class PaperScript2023:
 
     def results_01_individual(self):
         print("START: results_01_individual")
+
         def calculate_roc(params):
             path, model = params
             model_name, model = model
             print(f"results_01_individual calculate_roc {path} {model_name}")
             X_train, X_test, y_train, y_test = files_train_test_split(path)
+            # TODO make `runs` a parameter
             runs = 3
-            results_fit = multiple_runs(runs, model.fit, X_train, y_train)
-            results_roc = multiple_runs(runs, calculate_aucroc, model,X_test, y_test)
+            results_fit = list(multiple_runs(
+                runs, model.fit, X_train, y_train))
+            results_roc = list(multiple_runs(
+                runs, calculate_aucroc, model, X_test, y_test))
 
-            # results_fit = reduce(
-            #     lambda x,y: ite.chain(x,y), 
-            #     map(fit_partial, )
-            #     )
-            # time_fit, _ = elapsed_time(model.fit, X_train, y_train)
-            # model.fit(X_train, y_train)
-            # time_roc, roc = elapsed_time(calculate_aucroc, model,X_test, y_test)
-            # roc = calculate_aucroc(model,X_test, y_test)
-            return path, model_name, results_fit, results_roc
-            
+            result = {
+                'path': path,
+                'model_name': model_name,
+                'results_fit': results_fit,
+                'results_roc': results_roc
+            }
+            return result
+
         all_models = self.get_models()
         all_paths = self.get_paths(self.level)
         all_params = list(ite.product(all_paths, all_models))
         results = list(map(calculate_roc, all_params))
-        df = pd.DataFrame(results)
-        # df_final = self.convert_to_dataframe(rocs, level=self.level)
+
+        def convert(match_obj):
+            if match_obj.group(0) == '_':
+                return ""
+            if match_obj.group(0) == '/':
+                return " "
+
+        df = pd.DataFrame.from_dict(
+            {
+                (re.sub(r"_|/", convert , param['path'].strip()) , param['model_name']): 
+                    {
+                        "fit time": fit['run_details']['time_elapsed'], 
+                        "roc time": roc['run_details']['time_elapsed'],
+                        "roc value": roc['run_details']['fun_return']
+                    }
+                for param in results
+                for (fit, roc) in list(zip(param['results_fit'], param['results_roc']))
+
+            },
+            orient='index'
+        )
+
         self.write_df(df, filename='results_01_individual')
+        
         print("FINISH: results_01_individual")
 
     def results_02_tse(self):
         print("START: results_02_tse")
-        
+
         def local_get_tsne_results(params):
             path, model = params
             model_name, model = model
@@ -147,15 +183,15 @@ class PaperScript2023:
             X_train, X_test, y_train, y_test = files_train_test_split(path)
             x, y, hue = get_tsne_results(
                 model=model,
-                X=model.transform(X_test), 
-                y=y_test, 
+                X=model.transform(X_test),
+                y=y_test,
                 perplexity=self.perplexity
-                )   
+            )
             return path, model_name, x, y, hue
 
         def plot_tsne(params):
             path, model_name, x, y, hue = params
-            plt.figure(figsize=(16,10))
+            plt.figure(figsize=(16, 10))
             sns.scatterplot(
                 x=x, y=y,
                 hue=hue,
@@ -163,13 +199,14 @@ class PaperScript2023:
                 legend="full",
                 s=100
                 # alpha=0.3
-                )
+            )
             path_output = os.path.join(self.output, 'results_02_tse')
             if not os.path.exists(path_output):
                 os.makedirs(path_output)
-            filename = '_'.join(path.split(os.path.sep)[:-1][-(self.level+1):]) + "_" + model_name
+            filename = '_'.join(path.split(os.path.sep)[
+                                :-1][-(self.level+1):]) + "_" + model_name
             plt.savefig(os.path.join(path_output, f'{filename}.pdf'))
-            
+
         all_models = self.get_models()
         all_paths = self.get_paths(self.level)
         all_params = list(ite.product(all_paths, all_models))
@@ -178,53 +215,60 @@ class PaperScript2023:
         print("FINISH: results_02_tse")
 
     def results_03_combined(self):
-            print("START: results_03_combined")
+        print("START: results_03_combined")
 
-            def calculate_roc(params):
-                paths, model_name, model, split = params
-                train = paths[split[0]]
-                test = paths[split[1]]
-                print(f"results_03_combined calculate_roc {str(test)} {model_name}")
-                train_X_train, train_X_test, train_y_train, train_y_test = files_train_test_split_combined(train)
-                test_X_train, test_X_test, test_y_train, test_y_test = files_train_test_split_combined(test)
-                model.fit(train_X_train, train_y_train)
-                roc = calculate_aucroc(model, test_X_test, test_y_test)
-                return str(test[0]), model_name, roc
-            
-            def calculate_roc_combined(params):
-                path, model_params = params
-                model_name, model = model_params
-                print(f"results_03_combined calculate_roc_combined {path} {model_name}")
-                paths = np.array(glob.glob(os.path.join(path, f'*{os.sep}' )))
-                loo = LeaveOneOut()
-                splits = loo.split(paths) 
-                rocs = map(calculate_roc, [(paths, model_name, model, split) for split in splits])
-                return rocs
+        def calculate_roc(params):
+            paths, model_name, model, split = params
+            train = paths[split[0]]
+            test = paths[split[1]]
+            print(
+                f"results_03_combined calculate_roc {str(test)} {model_name}")
+            train_X_train, train_X_test, train_y_train, train_y_test = files_train_test_split_combined(
+                train)
+            test_X_train, test_X_test, test_y_train, test_y_test = files_train_test_split_combined(
+                test)
+            model.fit(train_X_train, train_y_train)
+            roc = calculate_aucroc(model, test_X_test, test_y_test)
+            return str(test[0]), model_name, roc
 
-            all_paths = self.get_paths(self.level - 1)
-            all_models = self.get_models()
-            all_params = list(ite.product(all_paths, all_models))
-            rocs = list(map(calculate_roc_combined, all_params))
-            rocs = reduce(lambda x,y: ite.chain(x,y), rocs)
-            rocs = list(rocs)
-            df_final = self.convert_to_dataframe(rocs, level=self.level -1)
-            self.write_df(df_final, filename='results_03_combined')
-            print("FINISH: results_03_combined")
+        def calculate_roc_combined(params):
+            path, model_params = params
+            model_name, model = model_params
+            print(
+                f"results_03_combined calculate_roc_combined {path} {model_name}")
+            paths = np.array(glob.glob(os.path.join(path, f'*{os.sep}')))
+            loo = LeaveOneOut()
+            splits = loo.split(paths)
+            rocs = map(calculate_roc, [
+                       (paths, model_name, model, split) for split in splits])
+            return rocs
+
+        all_paths = self.get_paths(self.level - 1)
+        all_models = self.get_models()
+        all_params = list(ite.product(all_paths, all_models))
+        rocs = list(map(calculate_roc_combined, all_params))
+        rocs = reduce(lambda x, y: ite.chain(x, y), rocs)
+        rocs = list(rocs)
+        df_final = self.convert_to_dataframe(rocs, level=self.level - 1)
+        self.write_df(df_final, filename='results_03_combined')
+        print("FINISH: results_03_combined")
+
 
 def main(parse_args):
     paper_script_2023 = PaperScript2023(**parse_args.__dict__)
     paper_script_2023.results()
-        
+
+
 if __name__ == "__main__":
     print("MTSA Pipeline")
     print("Initializing...")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sampling_rate',type=int)
+    parser.add_argument('--sampling_rate', type=int)
     parser.add_argument('--path', type=str)
     parser.add_argument('--n_components', type=int)
     parser.add_argument('--level', type=int)
     parser.add_argument('--perplexity', type=int)
     parser.add_argument('--output', type=str)
     parse_args = parser.parse_args()
-    main(parse_args)    
+    main(parse_args)
     print("MTSA Pipeline: Finished")
